b8de0dfbdc68e85ed32abb41b2bdca4e006c29b8
AlexKnauth <alexander@knauth.org>
2025-08-30 23:11:25 -0400

    Add segment_splitted

--- src/runtime/sys.rs
+++ src/runtime/sys.rs
@@ -86,6 +86,9 @@ extern "C" {
     /// finished, but has not been reset. So you need to be careful when using
     /// this value for indexing.
     pub fn timer_current_split_index() -> i32;
+    #[cfg(feature = "split-index")]
+    /// Whether the segment at `idx` was splitted this attempt.
+    pub fn timer_segment_splitted(idx: i32) -> bool;
     /// Sets a custom key value pair. This may be arbitrary information that the
     /// auto splitter wants to provide for visualization. The pointers need to
     /// point to valid UTF-8 encoded text with the respective given length.
