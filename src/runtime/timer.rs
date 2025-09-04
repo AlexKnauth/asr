39150fb229f4525ed2257cc2e26571401c5531ee
AlexKnauth <alexander@knauth.org>
2025-09-04 02:05:20 -0400

    features: split-index

--- src/runtime/timer.rs
+++ src/runtime/timer.rs
@@ -118,6 +118,7 @@ pub fn state() -> TimerState {
 /// when the attempt is finished, but has not been reset.
 /// So you need to be careful when using this value for indexing.
 /// Same index does not imply same split on undo and then split.
+#[cfg(feature = "split-index")]
 pub fn current_split_index() -> Option<u64> {
     let i = unsafe { sys::timer_current_split_index() };
     if i.is_negative() {
@@ -131,6 +132,7 @@ pub fn current_split_index() -> Option<u64> {
 /// or `Some(false)` if skipped.
 /// If `idx` is greater than or equal to the current split index,
 /// `None` is returned instead.
+#[cfg(feature = "split-index")]
 pub fn segment_splitted(idx: u64) -> Option<bool> {
     match unsafe { sys::timer_segment_splitted(idx) } {
         1 => Some(true),
