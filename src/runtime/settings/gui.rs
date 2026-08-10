88b29a63efb4803b8caac60aa712f5769bf5ad97
AlexKnauth <alexander@knauth.org>
2026-08-10 10:20:56 -0400

    clear push_str -> clone_into

--- src/runtime/settings/gui.rs
+++ src/runtime/settings/gui.rs
@@ -3,6 +3,9 @@
 
 use core::mem;
 
+#[cfg(feature = "alloc")]
+use alloc::borrow::ToOwned;
+
 #[cfg(feature = "derive")]
 pub use asr_derive::Gui;
 
@@ -435,8 +438,7 @@ impl Widget for TextInput {
         if let Some(value) = settings_map.get(key) {
             value.get_string_into(&mut self.value);
         } else {
-            self.value.clear();
-            self.value.push_str(args.default);
+            args.default.clone_into(&mut self.value);
         }
     }
 }
